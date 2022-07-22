c0ec54dcf6f072d0e44e95ab3e5718120d1022cf
Daniel Park <Danielpark1239@gmail.com>
2022-07-22 16:48:38 -0400

    added POST method to movies route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -18,22 +18,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
-
-@app.route('/test')
+    
+@app.route('/test/', methods=['GET', 'POST'])
 def test():
-    url = "https://www.rottentomatoes.com/m/thor_ragnarok_2017"
-    movie_html_text = requests.get(url).text
-    movieSoup = BeautifulSoup(movie_html_text, "lxml")
+    if request.method == "POST":
+        print(request.form.getlist("platform"))
+        print(request.form["tomatometerSlider"])
+        print(request.form["audienceSlider"])
+    return render_template("test.html")
 
-    availablePlatforms = movieSoup.find_all("where-to-watch-meta")
-    for platform in availablePlatforms:
-        print(platform["affiliate"])
-   
-    return render_template("index.html")
-
-
-@app.route('/movies/')
+@app.route('/movies/', methods=['GET', 'POST'])
 def get_movies():
+    if request.method == "POST":
+        print(request.form.getlist("genre"))
+        print(request.form.getlist("rating"))
+        print(request.form.getlist("platform"))
+        print(request.form["tomatometerSlider"])
+        print(request.form["audienceSlider"])
+        print(request.form["recommendationsNumber"])
+
     html_text = requests.get(
         url="https://www.rottentomatoes.com/browse/movies_at_home/sort:popular?page=1"
     ).text
@@ -41,13 +44,13 @@ def get_movies():
     movies = moviePageSoup.find_all(
         "a", 
         attrs={"href": re.compile("/m/"), "data-id": True}, 
-        limit=3 # LIMIT IS 5 FOR NOW
+        limit=1 # LIMIT IS 5 FOR NOW
     )
     movieInfo = {}
 
     desiredInfoCategories = [
             "Rating:", "Genre:", "Original Language:", "Release Date (Theaters):",
-            "Release Date (Streaming):", "Runtime:", "Distributor:"
+            "Release Date (Streaming):", "Runtime:"
         ]
 
     for movie in movies:
@@ -57,11 +60,8 @@ def get_movies():
         name = data.contents[-2].text.strip()
 
         movieInfo[name] = {
-            "audienceSentiment": scores["audiencesentiment"],
             "audienceScore": scores["audiencescore"],
-            "criticsSentiment": scores["criticssentiment"],
             "criticsScore": scores["criticsscore"],
-            "criticsCertified": True if scores["criticscertified"] else False
         }
 
         # Get additional data about the movie by looking at its page
@@ -89,8 +89,7 @@ def get_movies():
                 formattedInfo = info.next_sibling.next_sibling.text.strip()
                 formattedInfo = formattedInfo.replace(" ", "").replace("\n", "")
                 formattedInfo = formattedInfo.split(",")
-            elif info.text == desiredInfoCategories[2] or info.text == desiredInfoCategories[5] \
-            or info.text == desiredInfoCategories[6]:
+            elif info.text == desiredInfoCategories[2] or info.text == desiredInfoCategories[5]:
                 formattedInfo = info.next_sibling.next_sibling.text.strip()
             elif info.text == desiredInfoCategories[3] or info.text == desiredInfoCategories[4]:
                 date = info.next_sibling.next_sibling.text.split()
