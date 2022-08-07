cba3fcc542b804b06035f748ea538ed69d0ff58b
Daniel Park <Danielpark1239@gmail.com>
2022-08-07 18:51:56 -0400

    Removed flask_sse

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from worker import redis_url
-from flask_sse import sse
 
 app = Flask(__name__)
 app.config["REDIS_URL"] = redis_url
-app.register_blueprint(sse, url_prefix='/stream')
 
 if __name__ == '__main__':
     app.run()
