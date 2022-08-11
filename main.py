ea945abccaf716f3cfee1d4a82506c815da9b1bb
Daniel Park <Danielpark1239@gmail.com>
2022-08-11 15:58:37 -0400

    debugging producer cache

--- main.py
+++ main.py
@@ -471,7 +471,7 @@ def producerEnqueue():
     job = q.enqueue(
         scraper.scrapeDirectorProducer, filterData, "producer", result_ttl=86400
     )
-    job.meta["key"] = key
+    job.meta['key'] = key
     job.save_meta()
     return {"job_id": job.id}
 
