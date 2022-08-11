a4b8195c5dac4cf715777a937565a0d6c6991512
Daniel Park <Danielpark1239@gmail.com>
2022-08-11 09:39:18 -0400

    removed decode responses from worker

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ listen = ['high', 'default', 'low']
 
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
-conn = redis.from_url(redis_url, decode_responses=True)
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
