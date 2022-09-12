eef44418d6f370f0d6673efdd6d628ccedb4fa1a
Daniel Park <Danielpark1239@gmail.com>
2022-09-12 19:54:40 -0400

    Updated user-agent header

--- constants.py
+++ constants.py
@@ -17,8 +17,8 @@ BASE_MOVIE_HOME_URL = "https://www.rottentomatoes.com/browse/movies_at_home/"
 BASE_TV_URL = "https://www.rottentomatoes.com/browse/tv_series_browse/"
 
 # HTTP request headers to not run into "Access Denied", hopefully this works
-HEADERS = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36' } 
-
+HEADERS = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36' } 
+                           
 # Mapping from platform to correct URL representation
 URL_PLATFORM_DICT = {
     "amazon-prime-video-us": "amazon_prime",
