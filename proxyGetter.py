344c32c26a57c1cbb850fc42708bf09188b5fc0e
Daniel Park <Danielpark1239@gmail.com>
2022-09-12 20:16:57 -0400

    removed https

--- proxyGetter.py
+++ proxyGetter.py
@@ -4,15 +4,18 @@ import random
 
 # Returns a valid proxy from proxy.webshare.io
 def get_proxy():
-    PROXIES_URL = os.environ['PROXIES_URL']
+    PROXIES_URL = "https://proxy.webshare.io/api/v2/proxy/list/download/mtsihsevmmkmkcfraatxiiqwgazdbodfodlbdsgg/-/any/username/direct/-/" or os.environ['PROXIES_URL']
     proxiesResponse = requests.get(url=PROXIES_URL).text
     proxiesResponseList = proxiesResponse.splitlines()
+
+    print(proxiesResponseList)
     
     # Choose a random proxy
     proxy = random.choice(proxiesResponseList)
     print(f'Selected proxy: {proxy}')
 
     return {
-        'http': 'http://' + proxy,
-        'https': 'https://' + proxy
+        'http': 'http://' + proxy
     }
+
+get_proxy()
