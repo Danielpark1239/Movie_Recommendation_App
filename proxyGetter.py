519a4d1b5d145fdc6bdfb7ee3a3fed6afba14c24
Daniel Park <Danielpark1239@gmail.com>
2022-09-12 20:38:57 -0400

    Final ver

--- proxyGetter.py
+++ proxyGetter.py
@@ -6,14 +6,12 @@ import random
 def get_proxy():
     PROXIES_URL = os.environ['PROXIES_URL']
     proxiesResponse = requests.get(url=PROXIES_URL).text
-    proxiesResponseList = proxiesResponse.splitlines()
+    proxiesResponseList = proxiesResponse.split()
     
     # Choose a random proxy
     proxy = random.choice(proxiesResponseList)
     print(f'Selected proxy: {proxy}')
 
     return {
-        'http': 'http://' + proxy
+        'http': proxy
     }
-
-get_proxy()
