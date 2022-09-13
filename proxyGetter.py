2763cdc0b2cf93eb0a5b8cf3150d484c3a1d7025
Daniel Park <Danielpark1239@gmail.com>
2022-09-12 20:24:37 -0400

    Finalized proxy getter func

--- proxyGetter.py
+++ proxyGetter.py
@@ -4,11 +4,9 @@ import random
 
 # Returns a valid proxy from proxy.webshare.io
 def get_proxy():
-    PROXIES_URL = "https://proxy.webshare.io/api/v2/proxy/list/download/mtsihsevmmkmkcfraatxiiqwgazdbodfodlbdsgg/-/any/username/direct/-/" or os.environ['PROXIES_URL']
+    PROXIES_URL = os.environ['PROXIES_URL']
     proxiesResponse = requests.get(url=PROXIES_URL).text
     proxiesResponseList = proxiesResponse.splitlines()
-
-    print(proxiesResponseList)
     
     # Choose a random proxy
     proxy = random.choice(proxiesResponseList)
